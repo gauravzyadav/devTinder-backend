4dc59724bdf1a0aacc17bd49d5300c0ed46f7e55
Gaurav Yadav <gauravyadav@Gauravs-MacBook-Air.local>
2025-05-07 12:08:52 +0530

    multiple-route-handler

--- src/app.js
+++ src/app.js
@@ -2,31 +2,20 @@ const express = require("express");
 
 const app = express();
 
-//This will only handle the GET call to /user
-app.get("/user",(req,res) => {
-    res.send({firstname: "Gaurav" , lastname: "Yadav" });
-});
-//saving data to DB
-app.post("/user",(req,res) => {
-    res.send("Data saved succesfully to DB");
-});
-//
-app.delete("/user",(req,res) => {
-    res.send("Successfully deleted");
-});
-
-
-app.use("/hello",(req,res) => {
-    res.send("Hello hello 2");
-});
-
-app.use("/test",(req,res) => {
-    res.send("Hello form the test!");
-});
-
-app.use("/",(req,res) => {
-    res.send("Hello hello 3");
-});
+app.use("/user",
+    (req,res,next) => {                      //route handler
+    //res.send("1st response");               //route handler
+    next();                                   //route handler
+    },
+    (req,res,next) => {
+    //res.send("2nd response");
+    next();
+    },
+    (req,res,next) => {
+    res.send("3rd response");
+    ;
+    },
+);
 
 
 app.listen(3000, ()=> {
